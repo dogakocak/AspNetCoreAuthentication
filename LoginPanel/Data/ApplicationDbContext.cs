6f503481e985016b99c08b62c6feff54113c992a
dogakocak <dogakocak>
2023-11-05 12:19:57 +0300

    ApplicationDb created and Identity definitions on Program.cs

---
+++ LoginPanel/Data/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using LoginPanel.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace LoginPanel.Data;
+
+public class ApplicationDbContext : IdentityDbContext<User>
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+    {
+        
+    }
+}
--- LoginPanel/Program.cs
+++ LoginPanel/Program.cs
@@ -1,3 +1,8 @@
+using LoginPanel.Data;
+using LoginPanel.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +11,48 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<ApplicationDbContext>(options => 
+    options.UseMySQL("Server=127.0.0.1;User ID=root;Password=123456;Database=login_schema")
+);
+builder.Services.AddIdentity<User, IdentityRole>().AddEntityFrameworkStores<ApplicationDbContext>()
+    .AddDefaultTokenProviders();
+
+builder.Services.Configure<IdentityOptions>(options =>
+    {
+        options.Password.RequireDigit = false;
+        options.Password.RequireLowercase = false;
+        options.Password.RequireUppercase = false;
+        options.Password.RequiredLength = 6;
+        options.Password.RequireNonAlphanumeric = false;
+
+        options.Lockout.MaxFailedAccessAttempts = 5;
+        options.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes(5);
+        options.Lockout.AllowedForNewUsers = true;
+
+        options.User.RequireUniqueEmail = true;
+
+        options.SignIn.RequireConfirmedEmail = false;
+        options.SignIn.RequireConfirmedPhoneNumber = false;
+    }
+    );
+
+builder.Services.ConfigureApplicationCookie(options =>
+    {
+        options.LoginPath = "/account/login";
+        options.LogoutPath = "/account/logout";
+        options.AccessDeniedPath = "/account/accessdenied";
+        options.SlidingExpiration = true;
+        options.ExpireTimeSpan = TimeSpan.FromMinutes(36);
+
+        options.Cookie = new CookieBuilder()
+        {
+            HttpOnly = true,
+            Name = ".Loginapp.Security.Cookie",
+            SameSite = SameSiteMode.Strict
+        };
+    }
+    );
 
 var app = builder.Build();
 
